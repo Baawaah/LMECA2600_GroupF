e3afcbd6df86aa82d88e5f028f9e8baf1154b0df
S.TRAN <son.tran.be@gmail.Com>
2017-10-31 16:49:31 +0100

    Create Section_efficace.m

---
+++ Section_efficace.m
@@ -0,0 +1,8 @@
+function [sigma] = Section_efficace(X,Transfo,n_eV,User_Adress)
+% X, espèce chimique
+% Transfo, Fission/Capture
+% n_eV, energie du neutron incident
+% User_Adress, adresse de la DB.
+
+
+end
