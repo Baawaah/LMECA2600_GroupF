807f04e1eea2f8c9145f5b2788361495fce71805
Son <Son.tran.be@gmail.com>
2017-10-31 17:27:08 +0100

    maj du section effi

--- Section_efficace.m
+++ Section_efficace.m
@@ -2,7 +2,30 @@ function [sigma] = Section_efficace(X,Transfo,n_eV,User_Adress)
 % X, espèce chimique
 % Transfo, Fission/Capture
 % n_eV, energie du neutron incident
-% User_Adress, adresse de la DB.
+% User_Adress, adresse de la DB
+    item = 0;
+    X = 'Pu239'
+    Transfo = 'Fission'
+    switch X
+        case 'Pu239'
+            item = item + 1;
+        otherwise
+            item = item + 0;
+        
+    end
+    switch Transfo
+        case 'Capture'
+            item = item +10;
+        case 'Fission'
+            item = item +20
+        otherwise
+            item = item + 0;
+    end
 
+    if item == 21
+        A = load('data/Pu239F.txt');
+        loglog(A(:,1),A(:,2));
+        
 
+    end    
 end
