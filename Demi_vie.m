39e95e9fe1f138a34202817af03ad35f8b042d76
Son <Son.tran.be@gmail.com>
2017-11-06 17:43:51 +0100

    add demie vie

--- Demi_vie.m
+++ Demi_vie.m
@@ -1,23 +1,35 @@
 function demi_vie = Demi_vie (X,Transfo)
 % https://www.nndc.bnl.gov/nudat2/chartNuc.jsp
-    switch X
         Y = 365*24*60*60; % in [s]
         d = 24*60*60;     % in [s]
         h = 60*60;        % in [s]
         m = 60;           % in [s]
+    switch X
         case 'U235'
-            demi_vie=7.04 *10^8*Y;
+            if (Transfo == 'Alpha')
+                demi_vie=7.04 *10^8*Y;
+            end
         case 'U238'
-            demi_vie=4.468*10^9*Y;
+            if (Transfo == 'Alpha')
+                demi_vie=4.468*10^9*Y;
+            end
         case 'U239'
-            demi_vie=     23.45*m;
+            if (Transfo == 'BetaMinus')
+                demi_vie=     23.45*m;
+            end
         case 'Np239'
-            demi_vie=     2.356*d;
+            if (Transfo == 'BetaMinus')
+                demi_vie=     2.356*d;
+            end
         case 'Pu239'
-            demi_vie=     24110*Y;
+            if (Transfo == 'Alpha')
+                demi_vie=     24110*Y;
+            end
         case 'Xe135'
-            demi_vie=      9.14*h;
+            if (Transfo == 'BetaMinus')
+                demi_vie=      9.14*h;
+            end
         otherwise 
             disp('Element not in the list');    
-    end
+      end
 end
