a511414aab3f16aca5b9abbf386cacb7cc51bbd1
Son <Son.tran.be@gmail.com>
2017-11-27 10:07:02 +0100

    update

--- Section_efficace.m
+++ Section_efficace.m
@@ -1,38 +1,112 @@
-function [sigma] = Section_efficace(X,Transfo,n_eV,User_Adress)
-% X, espÃ¨ce chimique
-% Transfo, Fission/Capture
-% n_eV, energie du neutron incident en MeV
-% User_Adress, adresse de la DB
-% 
+function [sigma]=Section_efficace(X,Transfo,n_eV,Path)
+% Section_efficace [barn]  Section efficace d'une transformation pour 1 composant
+%   [SIGMA] = Section_efficace(X,TRANSFO,N_EV) donne la section 
+%   efficace SIGMA de la transformation TRANSFO de l'element chimique X
+%   lorsque le neutron incident a une énergie de N_EV
+%
+%   X : Espèce chimique.
+%       ATTENTION : ici on travail avec un nombre limité d'espèces
+%       chimiques
+%
+%   TRANSFO : seules les transformations ci dessous sont utilisées :
+%       Fission : Probabilité qu'un noyau absorbe un neutron et fissionne
+%       Capture : Probabilité qu'un noyau absorbe un neutron et fissionne 
+%
+%   n_eV : energie du neutron incident. Peut etre un vecteur
+%       ATTENTION : On suppose une energie comprise entre 1e-5 et 2e7 [eV]
+%
+%   Path : Adress of the data base
+%
+%   ETAPES : etapes intermédiaires pour construire la data base :
+%       1°) Construction d'une data base pour les elements Ux et Np9 et Pu9
+%       où Ux peuvent soit fissioner soit capturer jusqu'à U9. Puis U9 à
+%       Np9 peuvent soit fissionner soit beta -. Pu peut juste fissioner
+%
+%   SOURCES : fichier viennent de
+%   https://www-nds.iaea.org/exfor/endf.htm
+%   C'est la base ENDF qui a généralement été utilisé
 
-   if strcmp(X,'Pu239')
-    if strcmp(Transfo,'Capture')
-        disp('Capture Pu9 Not Possible');
-        return;
-    end
-   end
- 
+%% Check argument IN : 
 
-    filename = sprintf('/%s_%s.txt',X,Transfo);
-    filename = strcat(User_Adress,filename);
-    DATA = load(filename);    
-    
-    if n_eV > DATA(length(DATA)) || n_eV < DATA(length(1))
-        disp('n_eV out of database boundary');
+%On vérifie que l'espece chimique est dans nos database
+
+if nargin==0
+    n_eV=logspace(-5,6,10000);
+    Path='./DATABASE';
+    X='U235';
+    Transfo='Fission';
+elseif(n_eV~=sort(n_eV))
+    error('The asked cross sections vector must be sorted!');
+end
+
+switch X
+    case 'U235'
+    case 'U236'
+    case 'U237'
+    case 'U238'
+    case 'U239'
+    case 'Np239'
+    case 'Pu239'
+    case 'Xe135'
+    otherwise
+        sigma=0;
+        fprintf('\n WARNING : There is no database for element %s. \n Please check function information',X);
         return;
-    end
-    %% Chercheur et Interpolateur
-    flag = 0;
-    i = 1; 
-        while (i < length(DATA) && flag == 0) 
-            if  (DATA(i,1)*10^6<= n_eV  && n_eV <= DATA(i+1,1)*10^6)
-                a = (DATA(i+1,2)*10^6-DATA(i,2)*10^6)/(DATA(i+1,1)*10^6-DATA(i,1)*10^6);
-                b = (DATA(i+1,2)*10^6*DATA(i,1)*10^6 - DATA(i,2)*10^6*DATA(i+1,1)*10^6)/(DATA(i,1)*10^6-DATA(i+1,1)*10^6);
-                sigma = (a*n_eV + b)/10^6;
-                flag = 1;
-            end
-            i = i+1;
-        end
+end
+
+%On vérifie que le type de transformation existe :
+switch Transfo
+    case 'Capture'
+    case 'Fission'
+    otherwise
+        sigma=0;
+        fprintf('\n WARNING : There is no database for Transformation %s. \n Please check function information',Transfo);
+        return;
+end
+
+if (min(n_eV)<0.99e-5||max(n_eV)>2e7)
+    error('\n Neutron energy of %d in data base of %s is out of range : [1e-5 2e7]',n_eV,X);
+end
+
+%% Reading information in file
+
+switch Transfo
+    case 'Capture'
+        file_name=sprintf('%s/%s_CAPTURE.txt',Path,X);
+        [E,Sig,~]=textread(file_name,'%f%f%s');
+        
+    case 'Fission'
+        file_name=sprintf('%s/%s_FISSION.txt',Path,X);
+        [E,Sig,~]=textread(file_name,'%f%f%s');        
+
+    otherwise
+        error('Trou dans la raquette :/');
 end
 
 
+counter=1;
+sigma=0*n_eV;%Allcoation taille
+for ii=1:length(n_eV)% Il existe des absysses à plusiuuers ordonnées :/
+    for jj=counter:(length(E))
+        if(((n_eV(ii)-E(jj))/n_eV(ii))<10^-6)% Différence relative négligeable. Ceci a cause d'erreur d'arrondis :/
+            sigma(ii)=Sig(jj);
+            counter=jj;
+            break;
+        end
+        if(E(jj)>n_eV(ii))
+            sigma(ii)= (n_eV(ii)-E(jj-1))/(E(jj)-E(jj-1))*Sig(jj-1)+...
+                       (E(jj)-n_eV(ii))/(E(jj)-E(jj-1))*Sig(jj); 
+            counter=jj;
+            break;
+        end
+        
+    end
+end
+
+if nargin==0 
+    loglog(n_eV,sigma,'k') 
+end
+
+
+
+end
--- molarMass.m
+++ molarMass.m
@@ -1,27 +1,39 @@
-function M = molarMass(X)
+function M=molarMass(X)
+% M=molarMass(X) return the molar mass of element X
+%
+% INPUT : 
+% [X] : 'U235' , 'U238' , 'U239', 'Np239', 'Pu239', 'n'
+%
+% OUTPUT : 
+% M : Molar mass [kg/mol]
+% FROM : http://wwwndc.jaea.go.jp/NuC/
+if nargin == 0
+    X='U235';
+end
+
+switch X
+    case 'U235'
+        M=235.0439299/1000;%kg/mol
+    case 'U238'
+        M=238.0507826/1000;%kg/mol
+    case 'U239'
+        M=239.054294518/1000;%kg/mol
+    case 'Np239'
+        M=239.052940487/1000;%kg/mol
+    case 'Pu239'
+        M=239.052164844/1000;%kg/mol
+%     case 'PFstar' %assimilé à du Sn116 pour un soucis de cohérence energétique (voir exo 2.4)
+%         M=116.902956/1000;%kg/mol
+%     case 'PF' %assimilé à du Pd115
+%         M=114.913658506/1000;%kg/mol
+    case 'n'
+        M=1.00866491578/1000;%kg/mol
+        
+    otherwise
+        fprintf('\n ---------------- WARNING ----------------- \n No molar mass for species (%s)',X);
+        M=0;
+end
 
 
-% Input : [string] : nuclear element :
-% 'U235','U238','U239','Np239','Pu239','Xe135'.
 
-%Output : [double] : Molar mass of the element.
-%v1.0 31/10/17
-%%
-
-    switch X
-        case 'U235'
-            M=235.043931368;
-        case 'U238'
-            M=238.050789466;
-        case 'U239'
-            M=239.054294518;
-        case 'Np239'
-            M=239.052940487;
-        case 'Pu239'
-            M=239.052164844;
-        case 'Xe135'
-            M=134.907226844;
-        otherwise 
-            disp('Element not in the list');    
-    end
 end
