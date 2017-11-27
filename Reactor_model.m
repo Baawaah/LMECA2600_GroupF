83780e9ca094963f3675c7fdabc4e95a436f1f71
DotaLuxis <33262204+DotaLuxis@users.noreply.github.com>
2017-11-27 16:52:40 +0100

    Add files via upload

--- Reactor_model.m
+++ Reactor_model.m
@@ -1,7 +1,8 @@
 function [U5_burning_rate]= Reactor_model(t_final,n_th_init,mTot,U5_pour,U8_pour,Pu9_pour)
-
+ close all, clear all
 if nargin==0
-    n_eV=logspace(-5,6,10000);
+    n_eV_th=0.025;
+    n_eV_fast=10^6;
     Path='./DATABASE';
     X='U235';
     Transfo='Fission';
@@ -11,57 +12,75 @@ if nargin==0
     U5_pour=3/100;
     U8_pour=97/100 ;
     Pu9_pour=0;
-    
+    V_core=10; %m3;
 end
-phi_n=2200*n_th_init*10^-4;
-n_eV=0.025 %eV
+phi_n_th=(2200*n_th_init)/V_core;
+phi_n_fast=0; 
+% n_eV=0.025 %eV
 
-h=10^-4;   
-% z=0:h:t_final;
-z=linspace(0,10^-2,5)
+h=10^-2;  
+dt=h;
+z=0:h:t_final;
+Avogadro=6.022*10^23;
 
+Path='C:\Users\Pierre-Yves Legros\Documents\UCL\Nucléaire\Code\DATABASE';
 
 lambda = log(2)./[Demi_vie('U239','BetaMinus'),Demi_vie('Np239','BetaMinus'),Demi_vie('Pu239','Alpha')];
-sigma=10^-24.*[Section_efficace('U235','Fission',n_eV,Path),Section_efficace('U238','Fission',n_eV,Path),...
-    Section_efficace('U238','Capture',n_eV,Path),Section_efficace('U239','Fission',n_eV,Path),Section_efficace('Np239','Fission',n_eV,Path)...
-    Section_efficace('Pu239','Fission',n_eV,Path)];
+sigma_th=10^-28.*[Section_efficace('U235','Fission',n_eV_th,Path),Section_efficace('U238','Fission',n_eV_th,Path),...
+    Section_efficace('U238','Capture',n_eV_th,Path),Section_efficace('U239','Fission',n_eV_th,Path),Section_efficace('Np239','Fission',n_eV_th,Path)...
+    Section_efficace('Pu239','Fission',n_eV_th,Path)];
+sigma_fast = 10^-28*[Section_efficace('U235','Fission',n_eV_fast,Path),Section_efficace('U238','Fission',n_eV_fast,Path),...
+    Section_efficace('U238','Capture',n_eV_fast,Path),Section_efficace('U239','Fission',n_eV_fast,Path),Section_efficace('Np239','Fission',n_eV_fast,Path)...
+    Section_efficace('Pu239','Fission',n_eV_fast,Path)];
 
-sigma
-Path='C:\Users\Pierre-Yves Legros\Documents\UCL\NuclÃ©aire\Code\DATABASE';
 
-
-    function [y]= fun (t,x)
-        
-       y=zeros(7,1);
-       y(1)=-sigma(1)*phi_n*n_th_init*x(1)+lambda(3)*x(5);
-       y(2)=-sigma(2)*phi_n*n_th_init*x(2)-sigma(3)*phi_n*n_th_init*x(2);
-       y(3)=sigma(3)*phi_n*n_th_init*x(2)-sigma(4)*phi_n*n_th_init*x(3)-lambda(1)*x(3);
-       y(4)=lambda(1)*x(3)-sigma(5)*phi_n*n_th_init*x(4)-lambda(2)*x(4);
-       y(5)=lambda(2)*x(4)-sigma(6)*phi_n*n_th_init*x(5)-lambda(3)*x(5);
-       y(6)=-sigma(3)*phi_n*n_th_init*x(2);
-       y(7)=sigma(1)*phi_n*n_th_init*x(1) + sigma(2)*phi_n*n_th_init*x(2) + sigma(4)*phi_n*n_th_init*x(3) + sigma(5)*phi_n*n_th_init*x(4) + sigma(6)*phi_n*n_th_init*x(5);
-       %y;
-       %phi_n=2200*(x(6)+y(6));
+    function [y]= fun (~,x)
+       phi_n_th=2200*x(7)/V_core ;
+       phi_n_fast=1.4*10^7*x(8)/V_core;
+       y=zeros(8,1);
+       y(1)=-sigma_th(1)*phi_n_th*x(1)+lambda(3)*x(5)+2*(-sigma_fast(1)*phi_n_fast*x(1)+lambda(3)*x(5)); % U5
+       y(2)=-sigma_th(2)*phi_n_th*x(2)-sigma_th(3)*phi_n_th*x(2)+2*(-sigma_fast(2)*phi_n_fast*x(2)-sigma_fast(3)*phi_n_fast*x(2)); %U8
+       y(3)=sigma_th(3)*phi_n_th*x(2)-sigma_th(4)*phi_n_th*x(3)-lambda(1)*x(3)+2*(sigma_fast(3)*phi_n_fast*x(2)-sigma_fast(4)*phi_n_fast*x(3)-lambda(1)*x(3)); %U9
+       y(4)=lambda(1)*x(3)-sigma_th(5)*phi_n_th*x(4)-lambda(2)*x(4)+2*(lambda(1)*x(3)-sigma_fast(5)*phi_n_fast*x(4)-lambda(2)*x(4)); %Np9
+       y(5)=lambda(2)*x(4)-sigma_th(6)*phi_n_th*x(5)-lambda(3)*x(5)+2*(lambda(2)*x(4)-sigma_fast(6)*phi_n_fast*x(5)-lambda(3)*x(5)); %Pu9
+       y(6)=sigma_th(1)*phi_n_th*x(1) + sigma_th(2)*phi_n_th*x(2) + sigma_th(4)*phi_n_th*x(3) + sigma_th(5)*phi_n_th*x(4) + sigma_th(6)*phi_n_th*x(5)...
+           +2*(sigma_fast(1)*phi_n_fast*x(1) + sigma_fast(2)*phi_n_fast*x(2) + sigma_fast(4)*phi_n_fast*x(3) + sigma_fast(5)*phi_n_fast*x(4) + sigma_fast(6)*phi_n_fast*x(5)); %PF*
+       y(7)=sigma_th(1)*phi_n_th*x(1)-sigma_th(3)*phi_n_th*x(2)+sigma_th(2)*phi_n_th*x(2)+sigma_th(4)*phi_n_th*x(3)+sigma_th(5)*phi_n_th*x(4)+sigma_th(6)*phi_n_th*x(5);  %n_th
+       y(8)=sigma_fast(1)*phi_n_fast*x(1)+sigma_fast(2)*phi_n_fast*x(2)+sigma_fast(4)*phi_n_fast*x(3)+sigma_fast(5)*phi_n_fast*x(4)+sigma_fast(6)*phi_n_fast*x(5)%-sigma_fast(3)*phi_n_fast*x(2); %n_fast
     end
 
-C_0=[(U5_pour*mTot)/molarMass('U235'),(U8_pour*mTot)/molarMass('U238'),0,0,(Pu9_pour*mTot)/molarMass('Pu239'),n_th_init,0];
-X=ode15s(@fun,z,C_0);
+Avogadro=1;
+ C_0=[(U5_pour*mTot)/(molarMass('U235'))*Avogadro,(U8_pour*mTot)/(molarMass('U238'))*Avogadro,0,0,(Pu9_pour*mTot)/(molarMass('Pu239'))*Avogadro,0,10^10,10^10];
+ 
+ [t,C]=ode45(@(t,x) fun(t,x),z,C_0);
+ 
+figure
+subplot(3,3,1)
+plot(t,C(:,1)/Avogadro)
+title('Mole U5')
+subplot(3,3,2)
+plot(t,C(:,2)/Avogadro)
+title('Mole U8')
+subplot(3,3,3)
+plot(t,C(:,3)/Avogadro)
+title('Mole U9')
+subplot(3,3,4)
+plot(t,C(:,4)/Avogadro)
+title('Mole Np9')
+subplot(3,3,5)
+plot(t,C(:,5)/Avogadro)
+title('Mole Pu9')
+subplot(3,3,6)
+plot(t,C(:,6)/Avogadro)
+title('Mole PF*')
+subplot(3,3,7)
+plot(t,C(:,7))
+title('nombre neutron thermique')
+subplot(3,3,8)
+plot(t,C(:,8))
+title('nombre neutron fast')
+subplot(3,3,8)
+
+
 
 end
-
-
-%%
-% % It calculates ODE using Runge-Kutta 4th order method
-% y=zeros(length(z),5);
-% C_0=[(U5_pour*mTot)/molarMass('U235'),(U8_pour*mTot)/molarMass('U238'),0,0,(Pu9_pour*mTot)/molarMass('Pu239')];
-% y(1,:)=C_0;
-% 
-% for i=1:(length(z)-1)                              % calculation loop
-%     k_1 = fun(z(i),y(i,:));
-%     k_2 = fun(z(i)+0.5*h,y(i,:)+0.5*h*k_1);
-%     k_3 = fun((z(i)+0.5*h),(y(i,:)+0.5*h*k_2));
-%     k_4 = fun((z(i)+h),(y(i,:)+k_3*h));
-% 
-%     y(i+1,:) = y(i,:) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;  % main equation
-% end
-%%
