fde6b63bbbd6873a6d8b6ad3c5996723527b525f
DotaLuxis <33262204+DotaLuxis@users.noreply.github.com>
2017-12-04 17:33:14 +0100

    Add files via upload

--- Reactor_model.m
+++ Reactor_model.m
@@ -19,88 +19,105 @@ end
 phi_n_th=(2200*n_th_init)/V_core;
 phi_n_fast=0; 
 % n_eV=0.025 %eV
-
+bar_in=1; %pourcentage insertion des barres de contôle 
 h=10^-2;  
 dt=h;
 z=0:h:t_final;
 Avogadro=6.022*10^23;
 %% Directory path 
 Path='C:\Users\Pierre-Yves Legros\Documents\UCL\Nucléaire\Code\DATABASE';
-%% Calcul des demi-vies
+%% Calcul des demi-vies et sections efficaces 
 
-lambda = log(2)./[Demi_vie('U239','BetaMinus'),Demi_vie('Np239','BetaMinus'),Demi_vie('Pu239','Alpha'),5*10^-4,1];
 
-%% Calcul des sections efficaces (thermique et fast)
 
-sigma_th=10^-28.*[Section_efficace('U235','Fission',n_eV_th,Path),Section_efficace('U238','Fission',n_eV_th,Path),...
-    Section_efficace('U238','Capture',n_eV_th,Path),Section_efficace('U239','Fission',n_eV_th,Path),Section_efficace('Np239','Fission',n_eV_th,Path)...
-    Section_efficace('Pu239','Fission',n_eV_th,Path)]
-sigma_fast = 10^-28*[Section_efficace('U235','Fission',n_eV_fast,Path),Section_efficace('U238','Fission',n_eV_fast,Path),...
-    Section_efficace('U238','Capture',n_eV_fast,Path),Section_efficace('U239','Fission',n_eV_fast,Path),Section_efficace('Np239','Fission',n_eV_fast,Path)...
-    Section_efficace('Pu239','Fission',n_eV_fast,Path)]
+lambda_U9=log(2)/Demi_vie('U239','BetaMinus');
+lambda_Np9=log(2)/Demi_vie('Np239','BetaMinus');
+lambda_Pu9=log(2)/Demi_vie('Pu239','Alpha');
+lambda_Pfp=log(2)/Demi_vie('Xe135','BetaMinus');
+lambda_n=log(2)/(5*10^-4);
+lambda_Pf=log(2);
+lambda_rod = [50+bar_in*50,600+bar_in*1400] ;
 
-%% Fonction de l'évolution des espèces en fonction du temps
-    function [y]= fun (~,x)
-       phi_n_th=2200*x(7)/V_core ;
-       phi_n_fast=1.4*10^7*x(8)/V_core;
-       y=zeros(8,1);
-       y(1)=-(sigma_th(1)*phi_n_th+sigma_fast(1)*phi_n_fast)*x(1)+lambda(3)*x(5); % U5
-       y(2)=-(sigma_th(2)*phi_n_th+sigma_fast(2)*phi_n_fast)*x(2)-(sigma_th(3)*phi_n_th+sigma_fast(3)*phi_n_fast)*x(2); %U8
-       y(3)=(sigma_th(3)*phi_n_th+sigma_fast(3)*phi_n_fast)*x(2)-(sigma_th(4)*phi_n_th+sigma_fast(4)*phi_n_fast)*x(3)-lambda(1)*x(3); %U9
-       y(4)=-(sigma_th(5)*phi_n_th+sigma_fast(5)*phi_n_fast)*x(4)-lambda(2)*x(4)+lambda(1)*x(3); %Np9
-       y(5)=-(sigma_th(6)*phi_n_th+sigma_fast(6)*phi_n_fast)*x(5)-lambda(3)*x(5)+lambda(2)*x(4); %Pu9
-       y(6)=2*((sigma_th(1)*phi_n_th+sigma_fast(1)*phi_n_fast)*x(1) + (sigma_th(2)*phi_n_th+sigma_fast(2)*phi_n_fast)*x(2) + (sigma_th(4)*phi_n_th+sigma_fast(4)*phi_n_fast)*x(3)+...
-           + (sigma_th(5)*phi_n_th+sigma_fast(5)*phi_n_fast)*x(4) + (sigma_th(6)*phi_n_th+sigma_fast(6)*phi_n_fast)*x(5)); %PF*
-       y(7)=-sigma_th(1)*phi_n_th*x(1)-sigma_th(2)*phi_n_th*x(2)-sigma_th(4)*phi_n_th*x(3)-sigma_th(5)*phi_n_th*x(4)-sigma_th(6)*phi_n_th*x(5)-sigma_th(3)*phi_n_th*x(2)+...
-            lambda(4)*x(8)+lambda(5)*x(6);  %n_th
-       y(8)=(2*sigma_th(1)*phi_n_th+sigma_fast(1)*phi_n_fast)*x(1)+(2*sigma_th(2)*phi_n_th+sigma_fast(2)*phi_n_fast)*x(2)+(2*sigma_th(4)*phi_n_th+sigma_fast(4)*phi_n_fast)*x(3)+...
-           (2*sigma_th(5)*phi_n_th+sigma_fast(5)*phi_n_fast)*x(4)+(2*sigma_th(6)*phi_n_th+sigma_fast(6)*phi_n_fast)*x(5)-sigma_fast(3)*x(2)-lambda(4)*x(8); %n_fast
+sigma_U5f_th=10^-28*Section_efficace('U235','Fission',n_eV_th,Path);
+sigma_U5f_fast=10^-28*Section_efficace('U235','Fission',n_eV_fast,Path);
+sigma_U8f_th=10^-28*Section_efficace('U238','Fission',n_eV_th,Path);
+sigma_U8f_fast=10^-28*Section_efficace('U238','Fission',n_eV_fast,Path);
+sigma_U8a_th=10^-28*Section_efficace('U238','Capture',n_eV_th,Path);
+sigma_U8a_fast=10^-28*Section_efficace('U238','Capture',n_eV_fast,Path);
+sigma_U9f_th=10^-28*Section_efficace('U239','Fission',n_eV_th,Path);
+sigma_U9f_fast=10^-28*Section_efficace('U239','Fission',n_eV_fast,Path);
+sigma_Np9f_th=10^-28*Section_efficace('Np239','Fission',n_eV_th,Path);
+sigma_Np9f_fast=10^-28*Section_efficace('Np239','Fission',n_eV_fast,Path);
+sigma_Pu9f_th=10^-28*Section_efficace('Pu239','Fission',n_eV_th,Path);
+sigma_Pu9f_fast=10^-28*Section_efficace('Pu239','Fission',n_eV_fast,Path);
+sigma_Pfp_th=10^-28*Section_efficace('Xe135','Capture',n_eV_th,Path);
+sigma_Pfp_fast=10^-28*Section_efficace('Xe135','Capture',n_eV_fast,Path);
+coef_poison=0.05;
+
+
+
+%% Fonction à résoudre
+    function y =fun(~,x)
+        phi_n_th=2200*x(6)/V_core ;
+        phi_n_fast=1.4*10^7*x(7)/V_core;
+        
+        y=zeros(10,1);
+        
+        y(1)=lambda_Pu9*x(5)-(sigma_U5f_th*phi_n_th+sigma_U5f_fast*phi_n_fast)*x(1); %U5
+        y(2)=-(sigma_U8f_th*phi_n_th+sigma_U8f_fast*phi_n_fast+sigma_U8a_th*phi_n_th+sigma_U8a_fast*phi_n_fast)*x(2); %U8
+        y(3)=(sigma_U8a_th*phi_n_th+sigma_U8a_fast*phi_n_fast)*x(2)-(sigma_U9f_th*phi_n_th+sigma_U9f_fast*phi_n_fast)*x(3)-lambda_U9*x(3); %U9
+        y(4)=-(sigma_Np9f_th*phi_n_th+sigma_Np9f_fast*phi_n_fast)*x(4)-lambda_Np9*x(4)+lambda_U9*x(3); %Np9
+        y(5)=-(sigma_Pu9f_th*phi_n_th+sigma_Pu9f_fast*phi_n_fast)*x(5)+lambda_Np9*x(4)-lambda_Pu9*x(5); %Pu9
+        y(6)=lambda_n*x(7)-(sigma_U5f_th*phi_n_th*x(1)+sigma_U8f_th*phi_n_th*x(2)+sigma_U9f_th*phi_n_th*x(3)+sigma_Np9f_th*phi_n_th*x(4)+sigma_Pu9f_th*phi_n_th*x(5))...
+            -sigma_U8a_th*phi_n_th*x(2)+lambda_Pf*x(8)-lambda_rod(1)*x(6)-sigma_Pfp_th*phi_n_th*x(9); %n_th
+        y(7)=-lambda_n*x(7)+(2*sigma_U5f_th*phi_n_th+sigma_U5f_fast*phi_n_fast)*x(1)+(2*sigma_U8f_th*phi_n_th+sigma_U8f_fast*phi_n_fast)*x(2)+(2*sigma_U9f_th*phi_n_th+sigma_U9f_fast*phi_n_fast)*x(3)...
+            +(2*sigma_Np9f_th*phi_n_th+sigma_Np9f_fast*phi_n_fast)*x(4)+(2*sigma_Pu9f_th*phi_n_th+sigma_Pu9f_fast*phi_n_fast)*x(5)-sigma_U8a_fast*phi_n_fast*x(2)-lambda_rod(2)*x(7)-sigma_Pfp_fast*phi_n_fast*x(9);
+        y(8)=(1-coef_poison)*((2*sigma_U5f_th*phi_n_th+2*sigma_U5f_fast*phi_n_fast)*x(1)+(2*sigma_U8f_th*phi_n_th+2*sigma_U8f_fast*phi_n_fast)*x(2)+(2*sigma_U9f_th*phi_n_th+2*sigma_U9f_fast*phi_n_fast)*x(3)...
+            +(2*sigma_Np9f_th*phi_n_th+2*sigma_Np9f_fast*phi_n_fast)*x(4)+(2*sigma_Pu9f_th*phi_n_th+2*sigma_Pu9f_fast*phi_n_fast)*x(5))-lambda_Pf*x(8);
+        y(9)=coef_poison*((2*sigma_U5f_th*phi_n_th+2*sigma_U5f_fast*phi_n_fast)*x(1)+(2*sigma_U8f_th*phi_n_th+2*sigma_U8f_fast*phi_n_fast)*x(2)+(2*sigma_U9f_th*phi_n_th+2*sigma_U9f_fast*phi_n_fast)*x(3)...
+            +(2*sigma_Np9f_th*phi_n_th+2*sigma_Np9f_fast*phi_n_fast)*x(4)+(2*sigma_Pu9f_th*phi_n_th+2*sigma_Pu9f_fast*phi_n_fast)*x(5))-lambda_Pfp*x(9)-(sigma_Pfp_th*phi_n_th+sigma_Pfp_fast*phi_n_fast)*x(9);
+        y(10)=lambda_Pf*x(8)+lambda_Pfp*x(9)+(sigma_Pfp_th*phi_n_th+sigma_Pfp_fast*phi_n_fast)*x(9);
+       
     end
+
+
 
 %% Conditions initiales
 
 % Avogadro=1 ; 
- C_0=[(U5_pour*mTot)/(molarMass('U235'))*Avogadro,(U8_pour*mTot)/(molarMass('U238'))*Avogadro,0,0,(Pu9_pour*mTot)/(molarMass('Pu239'))*Avogadro,0,10^10,0];
+ C_0=[(U5_pour*mTot)/(molarMass('U235'))*Avogadro,(U8_pour*mTot)/(molarMass('U238'))*Avogadro,0,0,(Pu9_pour*mTot)/(molarMass('Pu239'))*Avogadro,(10^10),0,0,0,0];
 %% Méthode de résolution
  [t,C]=ode45(@(t,x) fun(t,x),z,C_0);
-  Avogadro=1 ; 
+%   Avogadro=1 ; 
 %% Plot des résultats 
-n=[0,t_final,-Inf,Inf]
+% n=[0,t_final,10^-15,10^3]
+loglog(t,C(:,5))
+
 
 figure
-subplot(3,3,1)
 loglog(t,C(:,1)/Avogadro)
-axis(n)
-title('Mole U5')
-subplot(3,3,2)
+hold on ;
+
 loglog(t,C(:,2)/Avogadro)
-axis(n)
-title('Mole U8')
-subplot(3,3,3)
+hold on ;
 loglog(t,C(:,3)/Avogadro)
-axis(n)
-title('Mole U9')
-subplot(3,3,4)
+hold on ;
 loglog(t,C(:,4)/Avogadro)
-axis(n)
-title('Mole Np9')
-subplot(3,3,5)
+hold on ;
 loglog(t,C(:,5)/Avogadro)
-axis(n)
-title('Mole Pu9')
-subplot(3,3,6)
+hold on ;
 loglog(t,C(:,6)/Avogadro)
-axis(n)
-title('Mole PF*')
-subplot(3,3,7)
-loglog(t,C(:,7))
-axis(n)
-title('nombre neutron thermique')
-subplot(3,3,8)
-loglog(t,C(:,8))
-axis(n)
-title('nombre neutron fast')
-subplot(3,3,8)
+hold on ;
+loglog(t,C(:,7)/Avogadro)
+hold on ;
+loglog(t,C(:,8)/Avogadro)
+hold on ;
+loglog(t,C(:,9)/Avogadro)
+hold on ;
+loglog(t,C(:,10)/Avogadro)
+% axis(n)
+legend('U5','U8','U9','Np9','Pu9','n_{th}','n_{fast}','PF*','PF_p','PF')
+hold off ; 
 
 
 
